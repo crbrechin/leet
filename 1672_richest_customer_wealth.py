85f883d7b055486b8ab65fe5f725cf7953005658
crbrechin <56694753+crbrechin@users.noreply.github.com>
2025-11-07 08:49:36 -0800

    Update 1672_richest_customer_wealth.py

--- 1672_richest_customer_wealth.py
+++ 1672_richest_customer_wealth.py
@@ -1,6 +1,7 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        m = []
-        for a in accounts:
-            m.append(sum(a))
-        return max(m)
+        m = [] # Declare empty array
+        for a in accounts: # For every customer
+            # Sum their wealth
+            m.append(sum(a)) # and put it in the array
+        return max(m) # Return the highest wealth
