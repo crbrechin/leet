634b912ef56a67e71c8a186d6e650ab8eb308da8
crbrechin <56694753+crbrechin@users.noreply.github.com>
2025-11-14 09:24:14 -0800

    Create 0175_combine_two_tables.sql

---
+++ 0175_combine_two_tables.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+Select Person.firstName as firstName, Person.lastName as lastName, Address.city as city, Address.state as state from Person
+    left join Address on Person.personId = Address.personId
