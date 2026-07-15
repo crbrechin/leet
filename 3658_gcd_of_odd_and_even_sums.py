e074f614317f28c410c8097aca2b6465a9e71ebd
crbrechin <56694753+crbrechin@users.noreply.github.com>
2026-07-15 14:38:31 -0700

    Create 3658_gcd_of_odd_and_even_sums.py

---
+++ 3658_gcd_of_odd_and_even_sums.py
@@ -0,0 +1,22 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        # if n == 1:
+        #     return 1
+        
+        # sumOdd, sumEven = 0, 0
+        # odd, even = 1, 2
+
+        # i = 0
+
+        # while i < n:
+        #     sumOdd += odd
+        #     sumEven += even
+            
+        #     odd += 2
+        #     even += 2
+        #     i += 1
+
+        # # print(f'{sumOdd}, {sumEven}') # DEBUG
+
+        # return sumEven % sumOdd
+        return n
