9f6e0c194588248d5927a3d92619b6be43794ad8
crbrechin <56694753+crbrechin@users.noreply.github.com>
2026-08-29 16:27:52 -0700

    Update 0268_missing_number.py

--- 0268_missing_number.py
+++ 0268_missing_number.py
@@ -1,5 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # print(len(nums)) # DEBUG
-        return [x for x in range(0,len(nums)+1) if x not in nums][0]
-        # print(i) # DEBUG
+        n = len(nums) + 1
+        m = [0] * n
+        for i in nums:
+            m[i] += 1
+        
+        return m.index(0)
