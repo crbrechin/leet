48036e24869da774110c026bee155216e67095b3
crbrechin <56694753+crbrechin@users.noreply.github.com>
2026-09-01 13:31:06 -0700

    Update 0001_two_sum.py

--- 0001_two_sum.py
+++ 0001_two_sum.py
@@ -1,20 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        o = [] # Output list
-        # Loop through the combinations
-        # until we find one that matches our target sum
-        for x,y in combinations(nums, 2):
-            if x + y == target: # Only one valid answer exists -- phew!
+        n = defaultdict(list)
+        
+        for i,z in enumerate(nums):
+            complement = target - z
+            # print(f'{i}, {z}') # DEBUG
+            
+            n[z].append(i)
 
-                # We want the index, not the value
-                i = nums.index(x)
-                o.append(i)
+            if complement == z:
+                if len(n[complement]) == 2:
+                    return [n[z][0], n[z][1]]
+            elif complement in n and complement != z:
+                return [n[z][0], n[complement][0]]
 
-                # x == y creates a doubled index
-                if x == y:
-                    j = nums.index(y, i + 1)
-                else:
-                    j = nums.index(y)
-                o.append(j)
-        # print(o) # DEBUG
-        return o
+        # print(f'{n}') # DEBUG
